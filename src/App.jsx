e2889a3e57c56790699d2c1740ffff8bd5d992ae
amandadr <amandadroy@gmail.com>
2024-05-25 20:37:41 -0300

    feat: HomePage component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import './App.css';
-import Hero from './components/Hero';
+import HomePage from './pages/HomePage';
 
 function App() {
   return (
     <div className="App">
-      <Hero />
+      <HomePage />
     </div>
   );
 }
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,11 @@
+import Hero from "../components/Hero";
+
+function HomePage() {
+  return (
+    <div className="HomePage">
+      <Hero />
+    </div>
+  );
+}
+
+export default HomePage;
