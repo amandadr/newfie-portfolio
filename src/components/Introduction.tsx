550a5492aef0472edc0619c65ac93609292b2ba8
amandadr <amandadroy@gmail.com>
2024-06-02 19:49:04 -0300

    refactor: add props to Introduction, update styling

--- src/components/Introduction.tsx
+++ src/components/Introduction.tsx
@@ -2,7 +2,6 @@ import React, { useEffect, useRef } from "react";
 import {
   Text,
   Card,
-  CardHeader,
   CardPreview,
   Image,
 } from "@fluentui/react-components";
@@ -10,10 +9,12 @@ import coppermine_2 from "images/coppermine_2.jpeg";
 import "../index.css";
 
 interface IntroductionProps {
+  header: string;
+  previewText: string;
   imageUrl: string;
 }
 
-const Introduction: React.FC<IntroductionProps> = ({ imageUrl }) => {
+const Introduction: React.FC<IntroductionProps> = ({ header, previewText, imageUrl }) => {
   const introductionRef = useRef<HTMLDivElement | null>(null);
 
   useEffect(() => {
@@ -44,26 +45,18 @@ const Introduction: React.FC<IntroductionProps> = ({ imageUrl }) => {
     };
   }, []);
 
-  const cardStyle = {
-    maxWidth: "80%",
-    padding: "48px",
-    backgroundColor: "rgba(255, 255, 255, 0.8)",
-    borderRadius: "32px",
-    position: "absolute" as "absolute",
-    top: "50%",
-    left: "50%",
-    transform: "translate(-50%, -50%)",
-  };
-
   return (
     <div
       ref={introductionRef}
-      style={{ position: "relative", height: "100dvh", minWidth: "100vw" }}
+      style={{ position: "relative", height: "100vh", minWidth: "100vw", display: "flex", alignItems: "center", justifyContent: "center"}}
     >
       <Image
         src={imageUrl || coppermine_2}
         fit="cover"
         style={{
+          position: "absolute",
+          zIndex: 1,
+          display: "flex",
           width: "100%",
           height: "100%",
           filter: "brightness(0.85)",
@@ -71,13 +64,33 @@ const Introduction: React.FC<IntroductionProps> = ({ imageUrl }) => {
           outlineOffset: "-0.5em",
         }}
       />
-      <Card style={cardStyle}>
-        <CardHeader header="Hi, I'm John Doe" />
-        <CardPreview>
-          <Text size={400}>
-            I'm a software engineer with 5 years of experience in web
-            development.
-          </Text>
+      <Card
+        size="medium"
+        style={{
+          zIndex: 2,
+          position: "relative",
+          maxHeight: "95%",
+          maxWidth: "95%",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "start",
+          justifyContent: "center",
+          padding: "2em",
+          backgroundColor: "rgba(30, 30, 30, 0.75)",
+          color: "whitesmoke",
+          borderRadius: "2em",
+        }}
+      >
+        <CardPreview
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            maxHeight: "95%",
+            gap: "1em",
+          }}
+        >
+          <Text size={700}>{header || "Header"}</Text>
+          <Text size={400}>{previewText || "This is Preview text."}</Text>
         </CardPreview>
       </Card>
     </div>
