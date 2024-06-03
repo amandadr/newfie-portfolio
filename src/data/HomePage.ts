2773640bb8f3a2bcbe8ed4c2630bbf5bd3c677af
amandadr <amandadroy@gmail.com>
2024-06-03 16:29:46 -0300

    style: include personal photos on HomePage

--- src/data/HomePage.ts
+++ src/data/HomePage.ts
@@ -1,6 +1,14 @@
+import butters_2 from "/images/butters_2.jpeg";
+import rock from "/images/rock.jpeg";
+import flower_2 from "/images/flower_2.jpeg";
+import flower_4 from "/images/flower_4.jpeg";
+import dingus_1 from "/images/dingus_1.jpeg";
+import butters_1 from "/images/butters_1.jpeg";
+
 export const homeLinks = [
   {
     imageUrl:
+      `${butters_2}` ||
       "https://images.pexels.com/photos/25000745/pexels-photo-25000745/free-photo-of-a-small-boat-docked-on-a-dock-near-a-small-town.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
     label: "About",
     target: "_self",
@@ -8,6 +16,7 @@ export const homeLinks = [
   },
   {
     imageUrl:
+      `${rock}` ||
       "https://images.pexels.com/photos/7575543/pexels-photo-7575543.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
     label: "Projects",
     target: "_self",
@@ -15,6 +24,7 @@ export const homeLinks = [
   },
   {
     imageUrl:
+      `${flower_2}` ||
       "https://images.pexels.com/photos/11542270/pexels-photo-11542270.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
     label: "Contact",
     target: "_self",
@@ -22,6 +32,7 @@ export const homeLinks = [
   },
   {
     imageUrl:
+      `${flower_4}` ||
       "https://images.pexels.com/photos/9011357/pexels-photo-9011357.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
     label: "LinkedIn",
     target: "_blank",
@@ -29,6 +40,7 @@ export const homeLinks = [
   },
   {
     imageUrl:
+      `${dingus_1}` ||
       "https://images.pexels.com/photos/11542288/pexels-photo-11542288.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
     label: "GitHub",
     target: "_blank",
@@ -36,6 +48,7 @@ export const homeLinks = [
   },
   {
     imageUrl:
+      `${butters_1}` ||
       "https://images.pexels.com/photos/11493011/pexels-photo-11493011.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
     label: "Blog\n(In Progress)",
     target: "_self",
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import Hero from "components/Hero";
 import Introduction from "components/Introduction";
-import Footer from "components/Footer";
 import NavContainer from "components/NavContainer";
+import Footer from "components/Footer";
 import { homeLinks } from "data/HomePage";
+import coppermine_2 from "/images/coppermine_2.jpeg";
+import gorge_2 from "/images/gorge_2.jpeg";
 import sky_1 from "/images/sky_1.jpeg";
 import "../index.css";
 
@@ -13,10 +15,10 @@ const HomePage: React.FC = () => {
   return (
     <div className="HomePage">
       <Hero
-        imageUrl="https://images.pexels.com/photos/11398315/pexels-photo-11398315.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+        imageUrl={coppermine_2}
         text="Whaddya hat, bh'y?"
       />
-      <Introduction header="Hi, I'm Amanda Roy" previewText="Welcome to my portfolio, where I hope you'll learn lots about my journey :)" imageUrl="https://images.pexels.com/photos/8607512/pexels-photo-8607512.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" />
+      <Introduction header="Hi, I'm Amanda Roy" previewText="Welcome to my portfolio, where I hope you'll learn lots about my journey :)" imageUrl={gorge_2} />
       <NavContainer
         navLinks={homeLinks}
       />
