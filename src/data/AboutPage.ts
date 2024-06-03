aa1cf66ab6e7d73c4c356d10ebcb3c4732204442
amandadr <amandadroy@gmail.com>
2024-06-02 23:56:25 -0300

    feat: implement modal functionality on experience nav

---
+++ src/data/AboutPage.ts
@@ -0,0 +1,32 @@
+export const expIcons = [
+  {
+    imageUrl: "/images/mac_search.jpeg",
+    label: "MacMillan Search",
+    details: {
+      date: "April, 2024 - Present",
+      header: "Junior SEO Specialist and Automation Developer",
+      content: "An SEO company.",
+      modalUrl: "/images/flower_1.jpeg",
+    },
+  },
+  {
+    imageUrl: "/images/da_tech.jpeg",
+    label: "DataAnnotation Tech",
+    details: {
+      date: "January, 2024 - Present",
+      header: "Machine Learning Research Developer",
+      content: "An LLM data company.",
+      modalUrl: "/images/flower_2.jpeg",
+    },
+  },
+  {
+    imageUrl: "/images/bootcamp.jpeg",
+    label: "Lighthouse Labs",
+    details: {
+      date: "September - December, 2023",
+      header: "Full Stack Web Development Graduate",
+      content: "A Canadian coding bootcamp.",
+      modalUrl: "/images/flower_4.jpeg",
+    },
+  },
+];
--- src/pages/AboutPage.tsx
+++ src/pages/AboutPage.tsx
@@ -1,9 +1,27 @@
 import React from "react";
+import Hero from "components/Hero";
+import Details from "components/Details";
+import Footer from "components/Footer";
+
+import fire from "/images/fire.jpeg";
+import flower_3 from "/images/flower_3.jpeg";
+import beach_2 from "/images/beach_2.jpeg";
+import me_1 from "/images/me_1.jpeg";
+import NavContainer from "@/components/NavContainer";
+import { expIcons } from "data/AboutPage";
 
 const AboutPage: React.FC = () => {
   return (
     <div className="AboutPage">
-      <h1>About</h1>
+      <Hero text="Who Knit You?" imageUrl={fire} />
+      <Details
+        header="About Me"
+        previewText="I am a software developer with a passion for learning. I am currently working on a project that will help people learn about how I code. It has nothing to do with knitting."
+        bgImage={beach_2}
+        highlightImage={me_1}
+      />
+      <NavContainer title="My Dev Experience:" navIcons={expIcons} />
+      <Footer imageUrl={flower_3} />
     </div>
   );
 };
