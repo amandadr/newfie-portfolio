ec9a025f0f839ddad56c4c5b0de8f517aa8e1078
amandadr <amandadroy@gmail.com>
2024-06-05 18:42:40 -0300

    feat: scaffold the contact page

---
+++ src/data/ContactPage.ts
@@ -0,0 +1,50 @@
+import flower_4 from "/images/flower_4.jpeg";
+import dingus_1 from "/images/dingus_1.jpeg";
+import butters_1 from "/images/butters_1.jpeg";
+import butters_2 from "/images/butters_2.jpeg";
+
+export const connectLinks = [
+  {
+    imageUrl:
+      `${butters_1}` ||
+      "https://images.pexels.com/photos/9011357/pexels-photo-9011357.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    label: "LinkedIn",
+    target: "_blank",
+    url: "https://www.linkedin.com/in/amandadroy/",
+  },
+  {
+    imageUrl:
+      `${dingus_1}` ||
+      "https://images.pexels.com/photos/11542288/pexels-photo-11542288.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    label: "GitHub",
+    target: "_blank",
+    url: "https://github.com/amandadr",
+  },
+  {
+    imageUrl:
+      `${butters_2}` ||
+      "https://images.pexels.com/photos/25000745/pexels-photo-25000745/free-photo-of-a-small-boat-docked-on-a-dock-near-a-small-town.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    label: "Discord",
+    target: "_blank",
+    url: "https://discordapp.com/users/amandadr",
+  },
+];
+
+export const workLinks = [
+  {
+    imageUrl:
+      `${butters_1}` ||
+      "https://images.pexels.com/photos/9011357/pexels-photo-9011357.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    label: "Resume",
+    target: "_blank",
+    url: "https://www.linkedin.com/in/amandadroy/",
+  },
+  {
+    imageUrl:
+      `${dingus_1}` ||
+      "https://images.pexels.com/photos/11542288/pexels-photo-11542288.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    label: "Email",
+    target: "_blank",
+    url: "mailto:amandadroy@gmail.com",
+  }
+];
--- src/pages/ContactPage.tsx
+++ src/pages/ContactPage.tsx
@@ -1,9 +1,28 @@
+import Hero from "components/Hero";
+import Details from "components/Details";
+import NavContainer from "components/NavContainer";
+import Footer from "components/Footer";
 import React from "react";
+
+import { connectLinks, workLinks } from "data/ContactPage";
 
 const ContactPage: React.FC = () => {
   return (
     <div className="ContactPage">
-      <h1>Contact</h1>
+      <Hero />
+      <Details
+        header="Thank you for being a friend :)"
+        previewText="If you'd like to connect with me, feel free to reach out through any of the following channels :D"
+        bgImage=""
+      />
+      <NavContainer navLinks={connectLinks} />
+      <Details
+        header="Looking good and need a solution?"
+        previewText="I'm open to hear you out and talk about building something awesome together!"
+        bgImage=""
+      />
+      <NavContainer navLinks={workLinks} />
+      <Footer />
     </div>
   );
 };
