c6b8733b2cc969700ce5c33fd1b6b836e44264e4
amandadr <amandadroy@gmail.com>
2024-06-07 00:12:02 -0300

    feat: add LogoNav to contact

--- src/pages/ContactPage.tsx
+++ src/pages/ContactPage.tsx
@@ -1,14 +1,18 @@
+import React, {useRef} from "react";
 import Hero from "components/Hero";
 import Details from "components/Details";
 import NavContainer from "components/NavContainer";
 import Footer from "components/Footer";
-import React from "react";
 
 import { connectLinks, workLinks } from "data/ContactPage";
+import LogoNav from "@/components/LogoNav";
 
 const ContactPage: React.FC = () => {
+  const footerRef = useRef<HTMLDivElement>(null);
+
   return (
     <div className="ContactPage">
+      <LogoNav footerRef={footerRef} currentPage="contact" />
       <Hero />
       <Details
         header="Thank you for being a friend :)"
@@ -22,7 +26,7 @@ const ContactPage: React.FC = () => {
         bgImage=""
         navLinks={workLinks}
       />
-      <Footer />
+      <Footer ref={footerRef} />
     </div>
   );
 };
