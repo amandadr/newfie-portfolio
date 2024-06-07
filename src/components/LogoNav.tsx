f4b4f2f61d6f868f92ec8694c89be4914b61ae8d
amandadr <amandadroy@gmail.com>
2024-06-06 23:32:14 -0300

    feat: add dim to LogoNav on scroll

--- src/components/LogoNav.tsx
+++ src/components/LogoNav.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import {
   Menu,
   MenuTrigger,
@@ -11,19 +11,38 @@ import {
 import { Button } from "@fluentui/react-button";
 
 const LogoNav: React.FC = () => {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 0) {
+        setIsScrolled(true);
+      } else {
+        setIsScrolled(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
     <div
       style={{
-        position: "absolute",
+        position: "fixed",
         zIndex: 25,
         width: "100%",
-        maxWidth: "100%%",
+        maxWidth: "100%",
         height: "100vh",
         maxHeight: "100vh",
         display: "flex",
         flexDirection: "column",
         justifyContent: "start",
         alignItems: "start",
+        transition: "opacity 0.5s ease",
+        opacity: isScrolled ? 0.8 : 1,
       }}
     >
       <Menu>
@@ -52,16 +71,24 @@ const LogoNav: React.FC = () => {
         <MenuPopover>
           <MenuList>
             <MenuItem key="home">
-              <Link appearance="subtle" href="/" >Home</Link>
+              <Link appearance="subtle" href="/">
+                Home
+              </Link>
             </MenuItem>
             <MenuItem key="about">
-              <Link appearance="subtle" href="/about">About</Link>
+              <Link appearance="subtle" href="/about">
+                About
+              </Link>
             </MenuItem>
             <MenuItem key="projects">
-              <Link appearance="subtle" href="/projects">Projects</Link>
+              <Link appearance="subtle" href="/projects">
+                Projects
+              </Link>
             </MenuItem>
             <MenuItem key="contact">
-              <Link appearance="subtle" href="/contact">Contact</Link>
+              <Link appearance="subtle" href="/contact">
+                Contact
+              </Link>
             </MenuItem>
           </MenuList>
         </MenuPopover>
