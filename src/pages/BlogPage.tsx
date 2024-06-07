bc6d51550c28f2aeeef2e2c6c67b400010d505ca
amandadr <amandadroy@gmail.com>
2024-06-06 22:31:41 -0300

    feat: add Footer to blog page

--- src/pages/BlogPage.tsx
+++ src/pages/BlogPage.tsx
@@ -1,3 +1,4 @@
+import Footer from "@/components/Footer";
 import Introduction from "@/components/Introduction";
 import React from "react";
 
@@ -5,6 +6,7 @@ const BlogPage: React.FC = () => {
   return (
     <div className="BlogPage">
       <Introduction header="Soon enough, a blog will sit here!" previewText="I'm working on it, I promise! It'll feature snack-sized updates on my projects, and maybe a few other things I find interesting, too!" imageUrl="" />
+      <Footer />
     </div>
   );
 };
