a1e22a8782457abedbbba501cdae8e60f6feb5e6
amandadr <amandadroy@gmail.com>
2024-06-15 23:56:14 -0300

    refactor: transition Modal to Next

--- src/app/components/Modal.tsx
+++ src/app/components/Modal.tsx
@@ -1,12 +1,19 @@
 "use client";
 import React from "react";
+// import {
+//   Card,
+//   CardPreview,
+//   CardFooter,
+//   Text,
+//   Link,
+// } from "@fluentui/react-components";
 import {
   Card,
-  CardPreview,
+  CardHeader,
+  CardBody,
   CardFooter,
-  Text,
   Link,
-} from "@fluentui/react-components";
+} from "@nextui-org/react";
 import Image from "next/image";
 import BouncingDots from "./BouncingDots";
 import Gallery from "./Gallery";
@@ -22,7 +29,7 @@ type ModalProps = {
   liveUrl?: string;
   githubUrl?: string; // Make GitHub URL optional as well
   techStack?: { class: string; name: string }[];
-}
+};
 
 const Modal: React.FC<ModalProps> = ({
   isOpen,
@@ -38,111 +45,56 @@ const Modal: React.FC<ModalProps> = ({
 }) => {
   return (
     <div
-    className="Modal relative w-full max-w-[100vw] flex flex-col justify-center items-center bg-[rgba(0,0,0,0.5)] z-10 p-20 gap-4"
+      className="Modal relative w-full max-w-[100vw] flex flex-col justify-center items-center bg-[rgba(0,0,0,0.5)] z-10 p-20 gap-4"
       onClick={onClose}
     >
       <Image
         alt="Modal"
         src={modalUrl || "flower_1.jpeg"}
         fill
-        style={{
-          zIndex: 1,
-          position: "absolute",
-          height: "100%",
-          width: "100%",
-          display: "flex",
-          filter: "blur(3px) opacity(0.75)",
-          cursor: "pointer",
-        }}
+        className="absolute object-cover w-full h-full z-1 filter-[blur(3px) opacity(0.75)] cursor-pointer"
       />
       <Card
-        size="medium"
-        style={{
-          zIndex: 2,
-          width: "80%",
-          padding: "2em",
-          backgroundColor: "white",
-          borderRadius: "1em",
-          cursor: "default",
-        }}
+        className="z-2 px-8 py-6 bg-white rounded-lg cursor-default"
         onClick={(e) => e.stopPropagation()}
       >
-        <CardPreview
-          style={{
-            display: "flex",
-            flexDirection: "row",
-            justifyContent: "space-around",
-            alignItems: "center",
-            marginBottom: "-1em",
-          }}
-        >
-          <Text size={300} weight="semibold">
-            {date}
-          </Text>
-          <div
-            style={{
-              display: "flex",
-              flexDirection: "row",
-              justifyContent: "space-around",
-              gap: "1em",
-            }}
-          >
+        <CardBody className="flex flex-row justify-between items-center gap-2">
+          <div className="font-medium">{date}</div>
+          <div className="w-80 flex flex-row justify-end gap-6">
             <Link
+              color="foreground"
               href={githubUrl}
               target="_blank"
-              appearance="subtle"
-              style={{
-                position: "relative",
-                display: "flex",
-                flexDirection: "row",
-                alignItems: "center",
-              }}
+              className="flex flex-row justify-around items-center gap-2"
             >
-              <i
-                className="devicon-github-plain"
-                style={{ fontSize: "2.5em", marginRight: "0.25em" }}
-              ></i>
-              <Text size={300}>GitHub Repo</Text>
+              <i className="devicon-github-plain text-[2.5em]"></i>
+              <div>GitHub Repo</div>
             </Link>
             {liveUrl && (
               <Link
+                color="foreground"
                 href={liveUrl}
                 target="_blank"
-                appearance="subtle"
-                style={{
-                  position: "relative",
-                  display: "flex",
-                  flexDirection: "row",
-                  alignItems: "center",
-                }}
+                className="flex flex-row justify-around items-center gap-2"
               >
-                <i
-                  className="devicon-chrome-plain"
-                  style={{ fontSize: "2.5em", marginRight: "0.25em" }}
-                ></i>
-                <Text size={300}>Live Site</Text>
+                <i className="devicon-chrome-plain text-[2.5em]"></i>
+                <div>Live Site</div>
               </Link>
             )}
           </div>
-        </CardPreview>
-        <CardPreview>
-          <Text size={700}>{header}</Text>
-          <Text size={400}>{content}</Text>
-        </CardPreview>
-        <CardFooter>
+        </CardBody>
+        <CardBody>
+          <div className="text-lg font-bold">{header}</div>
+          <div>{content}</div>
+        </CardBody>
+        <CardFooter className="gap-4">
           {techStack?.map((tech) => (
             <div
               key={tech.name}
-              className="flex flex-wrap"
-              style={{
-                display: "flex",
-                alignItems: "center",
-                justifyContent: "center",
-                gap: "0.5em",
-              }}
+              className="flex flex-wrap justify-center items-center gap-2"
             >
-              <i className={tech.class} style={{ fontSize: "1.5em" }}></i>
-              <Text size={300}>{tech.name}</Text>
+              <i className={tech.class + " text-[2em]"}></i>
+              <div className="text-small">{tech.name}</div>
             </div>
           ))}
         </CardFooter>
